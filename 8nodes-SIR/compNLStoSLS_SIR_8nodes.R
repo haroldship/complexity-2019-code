9dfd823e51c67892dbbea1bf3843ad7bd0b49b8b
Harold Ship <harold.ship@gmail.com>
2019-03-17 13:13:02 +0200

    update code for SIR

--- 8nodes-SIR/compNLStoSLS_SIR_8nodes.R
+++ 8nodes-SIR/compNLStoSLS_SIR_8nodes.R
@@ -1,19 +1,40 @@
 rm(list=ls())
+library(simode)
+set.seed(1000)
 
-pars <- c('beta1_1','beta1_2','beta2_1','beta2_2','gamma')
-vars <- c('S1_1','I1_1','S2_1','I2_1')
-S1_1 <- '-S1_1*(beta1_1*I1_1+beta1_2*I2_1)'
-I1_1 <- 'S1_1*(beta1_1*I1_1+beta1_2*I2_1)-gamma*I1_1'
-S2_1 <- '-S2_1*(beta2_1*I1_1+beta2_2*I2_1)'
-I2_1 <- 'S2_1*(beta2_1*I1_1+beta2_2*I2_1)-gamma*I2_1'
-equations <- c(S1_1,I1_1,S2_1,I2_1)
+S1_1 <- "-S1_1*(beta1_1*I1_1+beta1_2*I2_1)"
+S2_1 <- "-S2_1*(beta2_1*I1_1+beta2_2*I2_1)"
+S1_2 <- "-S1_2*kappa2*(beta1_1*I1_2+beta1_2*I2_2)"
+S2_2 <- "-S2_2*kappa2*(beta2_1*I1_2+beta2_2*I2_2)"
+S1_3 <- "-S1_3*kappa3*(beta1_1*I1_3+beta1_2*I2_3)"
+S2_3 <- "-S2_3*kappa3*(beta2_1*I1_3+beta2_2*I2_3)"
+S1_4 <- "-S1_4*kappa4*(beta1_1*I1_4+beta1_2*I2_4)"
+S2_4 <- "-S2_4*kappa4*(beta2_1*I1_4+beta2_2*I2_4)"
+S1_5 <- "-S1_5*kappa5*(beta1_1*I1_5+beta1_2*I2_5)"
+S2_5 <- "-S2_5*kappa5*(beta2_1*I1_5+beta2_2*I2_5)"
+I1_1 <- "S1_1*(beta1_1*I1_1+beta1_2*I2_1)-gamma*I1_1"
+I2_1 <- "S2_1*(beta2_1*I1_1+beta2_2*I2_1)-gamma*I2_1"
+I1_2 <- "S1_2*kappa2*(beta1_1*I1_2+beta1_2*I2_2)-gamma*I1_2"
+I2_2 <- "S2_2*kappa2*(beta2_1*I1_2+beta2_2*I2_2)-gamma*I2_2"
+I1_3 <- "S1_3*kappa3*(beta1_1*I1_3+beta1_2*I2_3)-gamma*I1_3"
+I2_3 <- "S2_3*kappa3*(beta2_1*I1_3+beta2_2*I2_3)-gamma*I2_3"
+I1_4 <- "S1_4*kappa4*(beta1_1*I1_4+beta1_2*I2_4)-gamma*I1_4"
+I2_4 <- "S2_4*kappa4*(beta2_1*I1_4+beta2_2*I2_4)-gamma*I2_4"
+I1_5 <- "S1_5*kappa5*(beta1_1*I1_5+beta1_2*I2_5)-gamma*I1_5"
+I2_5 <- "S2_5*kappa5*(beta2_1*I1_5+beta2_2*I2_5)-gamma*I2_5"
+
+#equations <- c(S1_1,S2_1,S1_2,S2_2,S1_3,S2_3,S1_4,S2_4,S1_5,S2_5,I1_1,I2_1,I1_2,I2_2,I1_3,I2_3,I1_4,I2_4,I1_5,I2_5)
+#vars <- c("S1_1","S2_1","S1_2","S2_2","S1_3","S2_3","S1_4","S2_4","S1_5","S2_5","I1_1","I2_1","I1_2","I2_2","I1_3","I2_3","I1_4","I2_4","I1_5","I2_5")
+equations <- c(S1_1, I1_1, S2_1, I2_1)
+vars <- c("S1_1", "I1_1", "S2_1", "I2_1")
 names(equations) <- vars
+
+pars <- c('beta1_1','beta1_2','beta2_1','beta2_2', 'gamma')
+
 theta <- c(6,2,1,3,2.333333)
 names(theta) <- pars
 x0 <- c(0.56,1e-4,0.57,1e-4)
 names(x0) <- vars
-library("simode")
-#sigma <- 0.05 compNLStoSLS1
 sigma <- c(0.05, 0.0005, 0.05, 0.0005)
 priorInf=c(0.1,1,3,5)
 
@@ -32,33 +53,33 @@ require(doParallel)
 registerDoParallel(cores=8)
 
 args <- c('equations', 'pars', 'time', 'x0', 'theta',
-         'nlin_pars', 'x_det', 'vars', 'sigma')
+          'nlin_pars', 'x_det', 'vars', 'sigma')
 
 for(ip in 1:4){
-
+  
   results <- foreach(j=1:N, .packages='simode', .export=args) %dorng% {
-  # for(j in 1:N) {
+    # for(j in 1:N) {
     obs <- list()
     for(i in 1:length(vars)) {
       obs[[i]] <- x_det[,i] + rnorm(n,0,sigma[i])
     }
     names(obs) <- vars
-  
-     nlin_init <- rnorm(length(theta[nlin_pars]),theta[nlin_pars],
-                        + priorInf[ip]*abs(theta[nlin_pars]))
-     names(nlin_init) <- nlin_pars
-     
-     ptimeNLS <- system.time({
-     NLSmc <- simode(equations=equations, pars=pars, fixed=x0, time=time, obs=obs,
-                     nlin_pars=nlin_pars, start=nlin_init, 
-                     im_method = "non-separable",
-                     simode_ctrl=simode.control(optim_type = "im"))})
-     ptimeSLS <- system.time({
-     SLSmc <- simode(equations=equations, pars=pars, fixed=x0, time=time, obs=obs,
-                     nlin_pars=nlin_pars, start=nlin_init,
-                     simode_ctrl=simode.control(optim_type = "im"))})
-     
-     list(NLSmc=NLSmc,SLSmc=SLSmc,ptimeNLS=ptimeNLS,ptimeSLS=ptimeSLS)
+    
+    nlin_init <- rnorm(length(theta[nlin_pars]),theta[nlin_pars],
+                       + priorInf[ip]*abs(theta[nlin_pars]))
+    names(nlin_init) <- nlin_pars
+    
+    ptimeNLS <- system.time({
+      NLSmc <- simode(equations=equations, pars=pars, fixed=x0, time=time, obs=obs,
+                      nlin_pars=nlin_pars, start=nlin_init, 
+                      im_method = "non-separable",
+                      simode_ctrl=simode.control(optim_type = "im"))})
+    ptimeSLS <- system.time({
+      SLSmc <- simode(equations=equations, pars=pars, fixed=x0, time=time, obs=obs,
+                      nlin_pars=nlin_pars, start=nlin_init,
+                      simode_ctrl=simode.control(optim_type = "im"))})
+    
+    list(NLSmc=NLSmc,SLSmc=SLSmc,ptimeNLS=ptimeNLS,ptimeSLS=ptimeSLS)
   }
   
   
